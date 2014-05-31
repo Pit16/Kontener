6e04220945a2704c74c82bad90fd0b1e639ce1fa
Pit16 <piotruss.168@gmail.com>
2014-05-31 16:51:27 +0200

    operator sturmieniowy dla Wezla
    zwracanie wartosci elementu

--- Wezel.cpp
+++ Wezel.cpp
@@ -1,8 +1,20 @@
 #include "Wezel.h"
 #include <iostream>
+using namespace std;
 Wezel::Wezel(int wartosc)
 {
     nastepny = NULL;
     poprzedni = NULL;
     this->wartosc=wartosc;
 }
+
+int Wezel::pokaz_wartosc()
+{
+    return wartosc;
+}
+ostream& operator<< (ostream& strumien, Wezel& wezel)
+ {
+   strumien<<"wartosc elementu wynosi"<<wezel.pokaz_wartosc()<<endl;
+
+   return strumien;
+ }
--- Wezel.h
+++ Wezel.h
@@ -1,5 +1,6 @@
 #ifndef WEZEL_H
 #define WEZEL_H
+#include <iostream>
 
 class Wezel
 {
@@ -8,8 +9,10 @@ public:
    ~Wezel();
     Wezel* nastepny;
     Wezel* poprzedni;
+    int pokaz_wartosc();
 private:
     int wartosc;
 };
+std::ostream& operator<< (std::ostream& strumien, Wezel& wezel);
 
 #endif //WEZEL_H
