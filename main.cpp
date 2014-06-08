d76a61541b00e749ec27463667945be54f62398f
Pit16 <piotruss.168@gmail.com>
2014-06-08 23:50:18 +0200

    Implementacja

--- main.cpp
+++ main.cpp
@@ -10,8 +10,12 @@ int main()
     nowy_kontener->dodaj_element(7);
     nowy_kontener->dodaj_element(3);
     nowy_kontener->dodaj_element(9);
+    cout<<*nowy_kontener<<endl;
+    nowy_kontener->usun_element(3);
     cout<<*nowy_kontener;
 
+    nowy_kontener->dodaj_element(5);
+    cout<<*nowy_kontener<<endl;
 
 
 	return 0;
