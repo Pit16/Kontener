aa4713ce941a08ea68f605d480ef30d7bec7bc31
Pit16 <piotruss.168@gmail.com>
2014-06-12 23:56:25 +0200

    Przyklady wykorzystania funkcjonalnosci

--- main.cpp
+++ main.cpp
@@ -8,30 +8,53 @@ int main()
 {
     cout<<"Hello world"<<endl;
 
-
-    Panstwo anglia("Anglia", 100, 1000);
-    Panstwo szwecja("Szwecja", 200, 3000);
-
-    ifstream plik2("testpanstwo");
-
-
-    Kontener<Panstwo> kontener_panstw;
-    plik2>>kontener_panstw;
-    cout<<kontener_panstw;
-
-
+    //tworzenie obiektu i dodanie elementow
     Kontener<int> kontener;
+    kontener.dodaj_element(0);
     kontener.dodaj_element(1);
     kontener.dodaj_element(2);
+    kontener.dodaj_element(3);
+    kontener.dodaj_element(4);
+    kontener.dodaj_element(5);
+    cout<<kontener<<endl;
+    //usuniecie obiektu
+    kontener.usun_element(2);
+    cout<<kontener<<endl;
+    //zamania obiektu
+    kontener.zamien_obiekty(2,4);
+    cout<<kontener<<endl;
+
+    //zastosowanie wyjatku
     try
     {
-        cout<<kontener.pobierz_element(1)<<endl;
+        cout<<kontener.pobierz_element(-3)<<endl;
     }
     catch(string wyjatek)
     {
             cout<<"Wyrzucono wyjatek: "<<endl;
             cout<<wyjatek<<endl;
     }
+
+    Panstwo anglia("Anglia", 100, 1000);
+    Panstwo szwecja("Szwecja", 200, 3000);
+    //zapis do pliku obiektu z pierwszego projektu
+    ofstream plik2("kpanstwo");
+
+    Kontener<Panstwo> kontenerPanstw;
+    kontenerPanstw.dodaj_element(anglia);
+    kontenerPanstw.dodaj_element(szwecja);
+    plik2<<kontenerPanstw;
+
+    ifstream plik3("kpanstwo");
+
+
+    Kontener<Panstwo> kontener_panstw;
+    plik3>>kontener_panstw;
+    cout<<kontener_panstw;
+
+
+
+
 
     return 0;
 }
